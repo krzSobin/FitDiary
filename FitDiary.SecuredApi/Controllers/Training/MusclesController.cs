ad7b16893e2ff5321aff8d722d42469ad717cb0a
PRACUJ\krzysztof.sobinski <krzysztof.sobinski@pracuj.pl>
2017-06-14 12:46:51 +0200

    muscles controller metody get

---
+++ FitDiary.SecuredApi/Controllers/Training/MusclesController.cs
@@ -0,0 +1,35 @@
+﻿using FitDiary.Contracts.DTOs.Training;
+using FitDiary.SecuredApi.Services.Training;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+
+namespace FitDiary.SecuredApi.Controllers.Training
+{
+    [EnableCors("*", "*", "*")]
+    [RoutePrefix("api/muscles")]
+    public class MusclesController : ApiController
+    {
+        private readonly MusclesService _musclesService = new MusclesService(); //TODO DI
+
+        // GET: api/muscles
+        [HttpGet]
+        [Route("")]
+        [ResponseType(typeof(IEnumerable<MuscleDTO>))]
+        public async Task<IEnumerable<MuscleDTO>> GetMusclesAsync()
+        {
+            return await _musclesService.GetMusclesAsync();
+        }
+
+        // GET: api/muscles/5
+        [HttpGet]
+        [Route("{muscleId:int}")]
+        [ResponseType(typeof(MuscleDTO))]
+        public async Task<MuscleDTO> GetMuscleAsync(int muscleId)
+        {
+            return await _musclesService.GetMuscleAsync(muscleId);
+        }
+    }
+}
---
+++ FitDiary.SecuredApi/Services/Training/MusclesService.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using FitDiary.Contracts.DTOs.Training;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace FitDiary.SecuredApi.Services.Training
+{
+    public class MusclesService
+    {
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["FitDiarySecuredApiContext"].ConnectionString; //TODO DI
+
+        public async Task<IEnumerable<MuscleDTO>> GetMusclesAsync()
+        {
+            using (IDbConnection con = new SqlConnection(_connectionString))
+            {
+                var sqlForExcercises = @"SELECT m.Id, m.Name
+                                         FROM [Muscles] m";
+                return await con.QueryAsync<MuscleDTO>(sqlForExcercises);
+            }
+        }
+
+        public async Task<MuscleDTO> GetMuscleAsync(int id)
+        {
+            using (IDbConnection con = new SqlConnection(_connectionString))
+            {
+                var sql = @"SELECT m.Id, m.Name
+                            FROM [Muscles] m
+                            WHERE m.Id = @Id";
+
+                return await con.QueryFirstOrDefaultAsync<MuscleDTO>(sql, new { Id = id });
+            }
+        }
+    }
+}
